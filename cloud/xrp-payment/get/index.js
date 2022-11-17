51e41c93807ea3594c790a1413d091c60e4f0795
siddharth <siddharth@apples-MacBook-Pro.local>
2022-11-17 21:55:26 +0530

    testnet

--- cloud/xrp-payment/get/index.js
+++ cloud/xrp-payment/get/index.js
@@ -59,7 +59,7 @@ module.exports = {
     const { txid } = params;
     if (exists(txid)) {
       try {
-        const Verify = new TxData();
+        const Verify = new TxData(["wss://s.altnet.rippletest.net:51233"]);
         const VerifiedResult = await Verify.getOne(txid);
         console.log("Verifide Result: ", VerifiedResult);
         return VerifiedResult;
